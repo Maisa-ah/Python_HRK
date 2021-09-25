44e1c1c9f3e4079790ef714bf631b6568efdbf86
Maisa Ahmad <2013maisaah@gmail.com>
2021-09-25 02:58:54 -0400

    improved code for deque

--- Collections/Deque.py
+++ Collections/Deque.py
@@ -1,12 +1,21 @@
+# from collections import deque
+# n = int(input())
+# d = deque()
+# for i in range(n):
+#     func, *val = input().split()
+#     if len(val)>0:
+#         getattr(d,func)(val[0])
+#     else:
+#         getattr(d,func)()
+# # print(d.count('1'))
+# for i in d:
+#     print(i, end=' ')
+
 from collections import deque
 n = int(input())
 d = deque()
 for i in range(n):
-    func, *val = input().split()
-    if len(val)>0:
-        getattr(d,func)(val[0])
-    else:
-        getattr(d,func)()
-# print(d.count('1'))
-for i in d:
-    print(i, end=' ')
+  func, *val = input().split()
+  getattr(d,func)(*val)
+# print(*[i for i in d])
+print(*d)
